8140024f0513f1b21696ecc5c1e74e73e1df180b
cauabrasil25 <cauabrasil25@gmail.com>
2023-10-17 18:20:33 -0300

    length

--- ListNat.hs
+++ ListNat.hs
@@ -1,6 +1,10 @@
 module ListNat where
-import Prelude hiding()
+import Prelude hiding(length)
 import Nat
 
 data ListNat = Empty | Cons Nat ListNat
     deriving ( Eq , Show )
+
+length :: ListNat -> Nat 
+length Empty = O 
+length (Cons x xs) = (S (length (xs)))
